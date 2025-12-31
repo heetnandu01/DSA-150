0463b6eac8d5c98faaa249493e35a0282a448c47
Heet <heetnandu01@gmail.com>
2025-12-31 18:20:52 +0530

    changed  filed name to avoid confusion of number files

--- Sprint-1/extra.java
+++ Sprint-1/extra.java
@@ -2,7 +2,7 @@
 
 import java.util.Scanner;
 
-public class twelve {
+public class extra{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the value of num: ");
