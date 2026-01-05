771f2d05465a639b39c3395ab494243ad43dc43c
Heet <heetnandu01@gmail.com>
2026-01-05 22:37:49 +0530

    solved three Problems

---
+++ patterns/fast-and-slow-pointers/five.java
@@ -0,0 +1,13 @@
+// leetcode Problem : https://leetcode.com/problems/middle-of-the-linked-list/
+
+// class five {
+//     public ListNode middleNode(ListNode head) {
+//         ListNode slow = head;
+//         ListNode fast = head;
+//         while (fast != null && fast.next != null) {
+//             slow = slow.next;
+//             fast = fast.next.next;
+//         }
+//         return slow;
+//     }
+// }
