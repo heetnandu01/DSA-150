25b6d15afc4c3b0baa6a04f89e17034aed443ff7
Heet <heetnandu01@gmail.com>
2026-02-10 00:14:51 +0530

    solved one problem

---
+++ patterns/In-place Reversal of a LinkedList/three.java
@@ -0,0 +1,21 @@
+// leetcode Problem : https://leetcode.com/problems/swap-nodes-in-pairs/description/
+
+// class three {
+//         public ListNode swapPairs(ListNode head) {
+//         ListNode dummy = new ListNode(0);
+//         dummy.next = head;
+
+//         ListNode prev = dummy;
+//         while(prev.next != null && prev.next.next != null){
+//             ListNode first = prev.next;
+//             ListNode second = first.next;
+
+//             first.next = second.next;
+//             second.next = first;
+//             prev.next = second;
+
+//             prev = first;
+//     }
+//         return dummy.next;
+//     }
+// }
