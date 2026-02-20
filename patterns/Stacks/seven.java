4b13a62d2bf70e5277679f3e8c10a4be3e47bcc5
Heet <heetnandu01@gmail.com>
2026-02-20 22:59:24 +0530

    solved one problem

---
+++ patterns/Stacks/seven.java
@@ -0,0 +1,30 @@
+// leetcode Problem :https://leetcode.com/problems/simplify-path/description/
+
+import java.util.Stack;
+
+class seven {
+     public String simplifyPath(String path) {
+        Stack<String> red = new Stack<>();
+        String[] blues = path.split("/");
+        for(String blue:blues){
+            if(blue.equals("") || blue.equals(".")){
+                continue;
+            }
+            if(blue.equals("..")){
+                if(!red.isEmpty()){
+                    red.pop();
+                }
+            }else{
+                red.push(blue);
+            }
+        }
+        if(red.isEmpty()){
+            return "/";
+        }
+        StringBuilder res = new StringBuilder();
+        for(String dir:red){
+            res.append("/").append(dir);
+        }
+        return res.toString();
+    }
+}
