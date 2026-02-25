5b5bc7845dfbc3a3b0177725d65e4e10244af155
Heet <heetnandu01@gmail.com>
2026-02-25 21:49:46 +0530

    solved three problems

---
+++ patterns/Binary search/four.java
@@ -0,0 +1,38 @@
+// GFG Problem : https://www.geeksforgeeks.org/problems/number-of-occurrence2259/1
+
+class four {
+      int countFreq(int[] arr, int target) {
+        int n = arr.length;
+        int low = 0;
+        int high = n-1;
+        int first = -1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(arr[mid] == target){
+                first = mid;
+                high = mid -1;
+            }else if(arr[mid] < target){
+                low = mid + 1;
+            }else{
+                high = mid - 1;
+            }
+        }
+        low = 0;
+        high = n-1;
+        int last = -1;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(arr[mid] == target){
+                last = mid;
+                low = mid + 1;
+            }else if(arr[mid] < target){
+                low = mid + 1;
+            }else{
+                high = mid - 1;
+            }
+        }
+        if(first == -1) return 0;
+        int result = last - first + 1;
+        return result;
+    }
+}
