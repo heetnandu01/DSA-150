e6d0b4bc25d4b91471313c5ec7166e0af18b8a2c
Heet <heetnandu01@gmail.com>
2026-02-26 21:46:08 +0530

    solved three problems today

---
+++ patterns/Binary search/eight.java
@@ -0,0 +1,17 @@
+// GFG Problem : https://www.geeksforgeeks.org/problems/rotation4723/1
+
+class eight {
+     public int findKRotation(int arr[]) {
+        int low = 0;
+        int high = arr.length - 1;
+        while(low < high){
+            int mid = low + (high - low)/2;
+            if(arr[mid] < arr[high]){
+                high = mid;
+            }else if(arr[mid] > arr[high]){
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+}
---
+++ patterns/Binary search/seven.java
@@ -0,0 +1,17 @@
+// leetcode problem : https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/description/
+
+class seven {
+      public int findMin(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+        while(low < high){
+            int mid = low + (high - low)/2;
+            if(nums[mid] > nums[high]){
+                low = mid + 1;
+            }else if(nums[mid] < nums[high]){
+                high = mid;
+            }
+        }
+        return nums[low];
+    }   
+}
