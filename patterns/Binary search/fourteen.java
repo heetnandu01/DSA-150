f2da8920ff0f7a9bbc97ebade8f6460214bac9f5
Heet <heetnandu01@gmail.com>
2026-03-01 21:45:10 +0530

    solved one problem today

---
+++ patterns/Binary search/fourteen.java
@@ -0,0 +1,26 @@
+//leetcode problem : https://leetcode.com/problems/maximum-candies-allocated-to-k-children/description/
+
+class fourteen {
+     public int maximumCandies(int[] candies, long k) {
+        int low = 1;
+        int answer = 0;
+        int high = 0;
+        for(int c:candies){
+            high = Math.max(high,c);
+        }
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            long pieces = 0;
+            for (int pile : candies) {
+                pieces += pile / mid;
+            }
+            if(pieces >= k){
+                answer = mid;
+                low = mid + 1;
+            }else {
+                high = mid -1;
+            }
+        }
+        return answer;
+    }
+}
