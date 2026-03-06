a76ba9c3d29bcc2a009d7f04d05403b23ab8f390
Heet <heetnandu01@gmail.com>
2026-03-06 20:33:26 +0530

    solved 2 problems today

---
+++ patterns/Binary search/nineteen.java
@@ -0,0 +1,17 @@
+// leetcode problem : https://leetcode.com/problems/search-a-2d-matrix-ii/
+
+public class nineteen {
+       public boolean searchMatrix(int[][] matrix, int target) {
+        int n=matrix.length;
+        int m=matrix[0].length;
+        int row=0, col=m-1;
+        while(row<n && col>=0)
+        {
+            if(matrix[row][col]==target)return true;
+            else if(matrix[row][col]<target)row++;
+            else
+            col--;
+        }
+        return false;
+    }
+}
