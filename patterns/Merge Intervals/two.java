93013ef61d159ba5234e38ae40c4e81f7b9fc6bd
Heet <heetnandu01@gmail.com>
2026-03-10 22:13:15 +0530

    solved one problem

---
+++ patterns/Merge Intervals/two.java
@@ -0,0 +1,38 @@
+// leetcode problem :https://leetcode.com/problems/insert-interval/description/
+
+import java.util.ArrayList;
+import java.util.List;
+
+class two {
+     public int[][] insert(int[][] intervals,int[] newInterval) {
+        int newStart = newInterval[0];
+        int newEnd = newInterval[1];
+        int n = intervals.length;
+        int i = 0;
+        List<int[]> list = new ArrayList<>();
+        while (i < n &&
+                newStart > intervals[i][1]) {
+
+            list.add(intervals[i]);
+            i++;
+        }
+        while (i < n &&
+                newEnd >= intervals[i][0]) {
+            newInterval[0] = Math.min(newInterval[0],
+                    intervals[i][0]);
+
+            newInterval[1] = Math.max(newInterval[1],
+                    intervals[i][1]);
+
+            i++;
+        }
+        list.add(newInterval);
+        while (i < n) {
+
+            list.add(intervals[i]);
+            i++;
+        }
+        return list.toArray(
+                new int[list.size()][]);
+    }
+}
