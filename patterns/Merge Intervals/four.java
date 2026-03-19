c027111079eeb21fb9ff222ccd07f47774f7e5a9
Heet <heetnandu01@gmail.com>
2026-03-19 20:50:23 +0530

    solved one problem

---
+++ patterns/Merge Intervals/four.java
@@ -0,0 +1,16 @@
+// GFG Problem : https://www.geeksforgeeks.org/problems/overlapping-intervals--174556/1
+
+import java.util.Arrays;
+
+class four {
+       static boolean isIntersect(int[][] intervals) {
+        // Code Here
+        Arrays.sort(intervals,(a,b) -> a[0] - b[0]);
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] <= intervals[i - 1][1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
