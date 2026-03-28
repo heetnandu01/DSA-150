50e3fd5fb9499c6c1cec13a3bf25d99a0cb20cbe
Heet <heetnandu01@gmail.com>
2026-03-28 23:48:07 +0530

    changes and fixes

--- Sprint-1/Eight.java
+++ Sprint-1/Eight.java
@@ -20,29 +20,3 @@
 
 
 
-import java.util.Scanner;
-
-public class Eight {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the value of num: ");
-        int num = sc.nextInt();
-        int rev = 0;
-        int original = num;
-
-        while(num > 0){
-            int digit = num % 10;
-            rev = rev * 10 + digit;
-            num = num / 10;
-        }
-
-        if(original == rev){
-            System.out.println("It is palindrome");
-        }else{
-            System.out.println("It is not a Palindrome");
-        }
-    }
-}
-
-
-
